4040f888de340f7bb6bd5eb5f62c8a923740efe4
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-08-13 11:42:53 +0200

    Creating default code for every constructor

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,32 @@ package src;
 // So if in the same dir no need for import?
 // import src.Test;
 
+class Potato {
+  public int x, y, z;
+
+  public Potato() {
+    // I wonder if cpp can do this? Just not this way probably
+    this(77, 66);
+  }
+
+  public Potato(int x, int y) {
+    this.x = x;
+    this.y = y; 
+  }
+  // Default code for every constructor
+  {
+    this.z = 99;
+  }
+}
+
 class Main {
 
   public static void main(String[] args) {
     // setting a ref to null tells the garbage collector to clean it
     Test test = new Test(7);
-    meth(test); // so objects are passe by refernece
-    System.out.printf("%d\n", test.x);
+    meth(test); // so objects are passed by refernece
+    Potato potato = new Potato();
+    System.out.printf("%d %d %d\n", potato.x, potato.y, potato.z);
   }
 
   public static void meth(Test test) {
